ed8f2508bc313f33fff4490a33ec5f1f3cd0b328
Akash Santhanam <akashkrish27@gmail.com>
2023-12-07 19:35:04 -0500

    Created React and Vite App with required dependencies

---
+++ src/Navbar.tsx
@@ -0,0 +1,15 @@
+function Navbar(){
+    return (
+        <div className = "navbar">
+            <h1>Toronto Drip</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shoes</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Cart</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar; 
